2793034ca4b8f263539d0253dc111e02603dc635
pxx917144686 <pxx917144686@MacBook-Pro.local>
2025-05-09 17:24:46 +0800

    删除根目录下的 .h 和 .m 文件

--- FLEXManager+Defaults.h
+++
@@ -1,8 +0,0 @@
-// 遇到问题联系中文翻译作者：pxx917144686
-#import "FLEXManager.h"
-
-@interface FLEXManager (Defaults)
-
-- (void)setupDefaultSettings;
-
-@end
--- FLEXToolbarItem.h
+++
@@ -1,9 +0,0 @@
-// 遇到问题联系中文翻译作者：pxx917144686
-#import <UIKit/UIKit.h>
-
-@interface FLEXToolbarItem : UIBarButtonItem
-
-+ (instancetype)itemWithTitle:(NSString *)title image:(UIImage *)image;
-- (NSArray *)toolbarItems;
-
-@end
